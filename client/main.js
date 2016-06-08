d203f16ff3185a0e78305a8f864fc98e58b5ef9f
Jeremy Iglehart <jeremy@autoschematic.com>
2016-06-07 23:31:32 -0400

    Initial Commit

---
+++ client/main.js
@@ -0,0 +1,37 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.date.helpers({
+  date() {
+    // Setup Date Variable
+    var now = new Date().getTime();
+
+    return now
+  },
+});
+
+Template.registerHelper('ISOToHuman', ( isoString ) => {
+  if ( isoString ) {
+    return moment( isoString ).format( 'MMMM Do, YYYY' );
+  }
+});
